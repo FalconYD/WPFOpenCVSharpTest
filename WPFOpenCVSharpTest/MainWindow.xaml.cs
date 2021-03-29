f59ad8d8b9ebc6b1407c4816009390282a1ab386
FalconYD <skg130176@gmail.com>
2021-03-29 17:30:28 +0900

    초기 커밋.

---
+++ WPFOpenCVSharpTest/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFOpenCVSharpTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Win_NewImage> listImage = new List<Win_NewImage>();
+        public int _nSelWin;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bn_Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            if(fdlg.ShowDialog() == true)
+            {
+                int id = listImage.Count;
+                //fdlg.FileName
+                listImage.Add(new Win_NewImage(this, id));
+                listImage[id].fn_OpenImage(fdlg.FileName);
+                listImage[id].Show();
+            }
+        }
+
+        private void bn_Zoom_In(object sender, RoutedEventArgs e)
+        {
+            double scale = listImage[_nSelWin].fn_GetScale();
+            listImage[_nSelWin].fn_Zoom(scale + 0.1);
+        }
+
+        private void bn_Zoom_Out(object sender, RoutedEventArgs e)
+        {
+            double scale = listImage[_nSelWin].fn_GetScale();
+            listImage[_nSelWin].fn_Zoom(scale - 0.1);
+        }
+
+        private void bn_Zoom_1x(object sender, RoutedEventArgs e)
+        {
+            listImage[_nSelWin].fn_Zoom(1.0);
+        }
+
+        private void bn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog fdlg = new SaveFileDialog();
+            if(fdlg.ShowDialog() == true)
+            {
+                listImage[_nSelWin].fn_SaveImage(fdlg.FileName);
+            }
+        }
+
+        private void bn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WPFOpenCVSharpTest/Win_NewImage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+
+namespace WPFOpenCVSharpTest
+{
+    /// <summary>
+    /// Win_NewImage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Win_NewImage : System.Windows.Window
+    {
+        int id = -1;
+        
+        public Win_NewImage(System.Windows.Window win, int id)
+        {
+            InitializeComponent();
+            this.Owner = win;
+            this.id = id;
+        }
+
+        public void fn_OpenImage(string strFile)
+        {
+            string strTitle = strFile.Substring(strFile.LastIndexOf("\\") + 1);
+            this.Title = strTitle;
+            Mat temp = Cv2.ImRead(strFile);
+            ImageBrush ib = new ImageBrush(WriteableBitmapConverter.ToWriteableBitmap(temp));
+            ib.Stretch = Stretch.None;
+
+            canvas.Width = ib.ImageSource.Width;
+            canvas.Height = ib.ImageSource.Height;
+            canvas.Background = ib;
+        }
+
+        public void fn_SaveImage(string strFile)
+        {
+            ImageBrush ib = canvas.Background as ImageBrush;
+            if(ib != null)
+            {
+                WriteableBitmap wb = ib.ImageSource as WriteableBitmap;
+                if (wb != null)
+                {
+                    Cv2.ImWrite(strFile, WriteableBitmapConverter.ToMat(wb));
+                }
+            }
+        }
+
+        public void fn_Zoom(double zoom)
+        {
+            myScaleTransform.ScaleX = zoom;
+            myScaleTransform.ScaleY = zoom;
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            ((MainWindow)Owner)._nSelWin = id;
+        }
+
+        public double fn_GetScale()
+        {
+            return myScaleTransform.ScaleX;
+        }
+    }
+}
